da3b68f88948afd80d013d95c297449158c140a1
Alexander <xanderzinho26@gmail.com>
2021-03-26 22:48:49 -0300

    :sparkles: GlobalStyles com tw

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,13 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
-import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro';
+import tw, { GlobalStyles as BaseStyles } from 'twin.macro';
 
 const CustomStyles = createGlobalStyle`
   * {
-    ${tw`m-0 p-0 box-border outline-none`}
+    ${tw`m-0 p-0 box-border outline-none no-underline`}
+    -webkit-font-smoothing: antialiased;
+    list-style: none;
+    font-family: 'Averta-Regular',Helvetica,sans-serif;
   }
   body {
-    -webkit-tap-highlight-color: ${theme`colors.purple.500`};
-    ${tw`antialiased bg-gray-50`}
+    ${tw`text-xl leading-8`}
   }
 `;
 
