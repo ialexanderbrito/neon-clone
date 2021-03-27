9e7a728f56e4c6bf622fbcd8f62f15ab07659a6e
Alexander <xanderzinho26@gmail.com>
2021-03-26 22:44:16 -0300

    :wrench: Configuração de fontes e nome do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,30 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './styles/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './styles/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'class', // false or 'media' or 'class'
   theme: {
     fontFamily: {
-      'inter': ['Inter', 'sans-serif'],
+      averta: ['Averta', 'sans-serif'],
+      inter: ['Inter', 'sans-serif'],
+      roboto: ['Roboto', 'sans-serif'],
     },
     extend: {
-      textOpacity: ['dark']
+      textOpacity: ['dark'],
     },
     screens: {
-      'xs': { 'min': '320px', 'max': '639px' },
-      'sm': { 'min': '640px', 'max': '767px' },
-      'md': { 'min': '768px', 'max': '1023px' },
-      'lg': { 'min': '1024px', 'max': '1279px' },
-      'xl': { 'min': '1280px', 'max': '1535px' },
-      '2xl': { 'min': '1536px' },
+      xs: { min: '320px', max: '639px' },
+      sm: { min: '640px', max: '767px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { min: '1024px', max: '1279px' },
+      xl: { min: '1280px', max: '1535px' },
+      '2xl': { min: '1536px' },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
