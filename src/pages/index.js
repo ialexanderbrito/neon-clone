597eba721749db6379bd3f6ae20d93add248c4c1
Alexander <xanderzinho26@gmail.com>
2021-03-26 22:49:37 -0300

    :sparkles: Página Home menu, head e vídeo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,95 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
-import { Wrapper, Button } from '../styles/Home';
+import Button from '../components/Button';
+
+import {
+  Container,
+  ContentMenu,
+  ContentBanner,
+  Main,
+  ContainerMovie,
+} from '../styles/Home';
 
 export default function Home() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      setScrollPosition(window.scrollY);
+    });
+  }, [scrollPosition]);
   return (
     <>
       <Head>
-        <title>Template TailwindCSS & Styled Components</title>
+        <title>
+          Conta Digital Grátis e Cartão de Crédito sem anuidade | Neon
+        </title>
       </Head>
-      <Wrapper>
-        <Button
-          onClick={() => {
-            alert('@ialexanderbrito');
-          }}
-        >
-          {' '}
-          Touch me
-        </Button>
-      </Wrapper>
+      <Container>
+        <ContentMenu scrollPosition={scrollPosition}>
+          <h1>
+            <img src="assets/logo.svg" alt="Banco Neon" />
+            <title>Logotipo Neon</title>
+          </h1>
+
+          <ul>
+            <li>
+              <a href="/">Conta</a>
+            </li>
+            <li>
+              <a href="/">Pejota</a>
+            </li>
+            <li>
+              <a href="/">Perguntas</a>
+            </li>
+            <li>
+              <a href="/">Tarifas</a>
+            </li>
+            <li>
+              <a href="/">#FocoNoDinheiro</a>
+            </li>
+            <li>
+              <a href="/">Conheça a Neon</a>
+            </li>
+            <li>
+              <Button>Abra sua conta</Button>
+            </li>
+          </ul>
+        </ContentMenu>
+        <ContentBanner>
+          <article>
+            <header>
+              <h1>A conta digital certa pra cuidar bem do seu dinheiro</h1>
+              <p>
+                Neon, conta grátis e cartão sem anuidade. Abra sua conta, é só
+                baixar o app!
+              </p>
+              <p>
+                <Button>Abra sua conta</Button>
+              </p>
+            </header>
+            <img src="assets/banner.png" alt="App mobile" />
+          </article>
+        </ContentBanner>
+        <Main>
+          <ContainerMovie>
+            <header>
+              <h1 style={{ display: 'none' }}>Video de campanha Neon</h1>
+            </header>
+            <div className="container-video">
+              <iframe
+                title="Video de campanha Neon: Windersson Blogueira"
+                src="https://www.youtube.com/embed/cEM7Ff_d1BU?controls=0&enablejsapi=1&origin=https%3A%2F%2Fneon.com.br"
+                frameBorder={0}
+                width={710}
+                height={399}
+                allowFullScreen
+              />
+            </div>
+          </ContainerMovie>
+        </Main>
+      </Container>
     </>
   );
 }
--- src/styles/Home.js
+++ src/styles/Home.js
@@ -1,27 +1,112 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import tw from 'twin.macro';
 
 export const Wrapper = styled.div`
   ${tw`flex items-center justify-center min-h-screen w-screen`};
 `;
 
-export const Button = styled.button`
-  ${tw`
-    bg-gradient-to-r from-green-400 to-blue-500
-    hover:from-green-500 hover:to-blue-600
-    w-2/12
-    h-12
-    xs:w-4/12
-    sm:w-4/12
-    md:w-4/12
-    font-inter
-    xs:text-xs
-    sm:text-sm
-    md:text-base
-    text-xl
-    font-semibold
-    text-white
-    rounded-md
-    shadow-lg
-    `};
+export const Container = styled.div`
+  ${tw`w-full h-full pb-5`}
+`;
+
+export const ContentMenu = styled.header`
+  ${tw`w-full h-24 py-0 px-4 fixed flex items-center bg-transparent`}
+  z-index: 1000;
+  transition: all 0.35s ease;
+
+  ${(props) =>
+    props.scrollPosition >= 52 &&
+    css`
+      background-color: #fff;
+      height: 70px;
+    `}
+
+  h1 {
+    ${tw`h-10`}
+    flex-basis: 240px;
+
+    img {
+      filter: brightness(0) invert(1);
+
+      ${(props) =>
+    props.scrollPosition >= 52 &&
+    css`
+          filter: initial;
+        `}
+    }
+  }
+
+  ul {
+    ${tw`flex items-center justify-end flex-wrap flex-1 h-10`}
+
+    li:first-child {
+      ${tw`ml-0`}
+    }
+
+    li {
+      ${tw`ml-8`}
+
+      a {
+        ${tw`font-bold text-xl leading-8 text-white font-inter`}
+
+        ${(props) =>
+    props.scrollPosition >= 52 &&
+    css`
+            color: #00a5f0;
+          `}
+      }
+    }
+  }
+`;
+
+export const ContentBanner = styled.section`
+  ${tw`pt-28 pr-0 pb-6 pr-0`}
+  min-height: 380px;
+  background: #0073cd;
+
+  article {
+    ${tw`max-w-6xl m-0 m-auto flex items-center`}
+  }
+
+  header {
+    ${tw`flex flex-col w-full h-80 my-0 mr-0 ml-24 pt-0 pr-4 pb-0`}
+    max-width: 524px;
+  }
+
+  h1 {
+    ${tw`text-5xl text-white text-left font-bold font-inter`}
+    flex-wrap: bold;
+    line-height: 60px;
+  }
+
+  p {
+    ${tw`mt-5 text-white`}
+  }
+
+  p + p {
+    ${tw`w-48`}/* width: 179px; */
+  }
+
+  img {
+    ${tw`w-full `}
+    max-width: 505px;
+  }
+`;
+
+export const Main = styled.main`
+  ${tw`w-full mt-10 mr-0 mb-0`}
+`;
+
+export const ContainerMovie = styled.article`
+  ${tw`max-w-6xl w-full flex items-center justify-center m-0 m-auto py-0 px-7`}
+
+  .container-video {
+    ${tw`mb-5`}
+    width: 740px;
+    height: 399px;
+
+    iframe {
+      ${tw`rounded-xl`}
+    }
+  }
 `;
